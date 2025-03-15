742531b6e7f69efa0f40c4d1cfe6479ec327ff09
aguinaldo-neto <neto_aguinaldo@outloook.com>
2025-03-15 13:47:46 -0300

    cria view de Esqueceu sua senha

--- front-end/personal-api/src/app/modules/auth/auth.routes.ts
+++ front-end/personal-api/src/app/modules/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
 import { AuthComponent } from "./components/auth-access/auth.component";
 import { SignupComponent } from "./components/signup/signup.component";
+import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
 
 
 export const AUTH_ROUTES: Routes = [
@@ -14,7 +15,11 @@ export const AUTH_ROUTES: Routes = [
     component: ForgotPasswordComponent
   },
   {
+    path: 'reset-password', // Rota: /auth/reset-password
+    component: ResetPasswordComponent
+  },
+  {
     path: 'signup', // Rota: /auth/signup
     component: SignupComponent
-  }
+  },
 ];
--- front-end/personal-api/src/app/modules/auth/components/forgot-password/forgot-password.component.ts
+++ front-end/personal-api/src/app/modules/auth/components/forgot-password/forgot-password.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
 
+  sendPasswordForm: FormGroup;
+
+  constructor(private form: FormBuilder) {
+    this.sendPasswordForm = this.form.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.sendPasswordForm.valid) {
+      console.log('Form submitted', this.sendPasswordForm.value);
+
+      // Call API to send password reset link
+      // inserir mensagem genérica de sucesso
+    }
+  }
 }
